df999489d70074c51ac7183035e77d3f82cfbac2
likelion_minhyeok <dufflaa@gmail.com>
2016-04-20 06:18:01 +0000

    saveall

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,36 @@
+require 'mailgun'
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def write
+    @title = params[:title]
+    @content = params[:content]
+    
+    #메일 보내는 부분
+    mg_client = Mailgun::Client.new("key-c1945760b48b93f3199ddaecaeac0e0e")
+    message_params =  {
+                      from: 'MHc9mailgun@likelion.com',
+                      to:   'ekerll@nate.com',
+                      subject: @title,
+                      text:    @content
+                      }
+    result = mg_client.send_message('sandboxbabdd8bd134d4d54aa3dcca3dc133da3.mailgun.org', message_params).to_h!
+    message_id = result['id']
+    message = result['message']
+    
+    #DB에 저장하는 부분
+    new_post = Post.new
+    new_post.content = params[:content]
+    new_post.title = params[:title]
+    new_post.save
+    
+    redirect_to "/list"
+  end
+  
+  def list
+    @every_post = Post.all.order("id desc")
+                                #asc는 올바른 순서. 기본셋팅
+  end
+  
+end
